e1b30ba2ffcc772fffb8c819d608ff70a39ac286
Christopher Burk <c.burk.tech@gmail.com>
2021-11-10 01:55:58 -0800

    adds findMax method and appropriate tests to test suite, updates README file, completes challenge

--- javascript/trees/__tests__/binary-search-tree.test.js
+++ javascript/trees/__tests__/binary-search-tree.test.js
@@ -6,12 +6,14 @@ describe('Testing Binary Search Tree', () => {
 
   it('Can instantiate an empty binary search tree', () => {
     const tree = new BinarySearchTree();
+
     expect(tree.root).toStrictEqual(null);
   });
 
   it('Can instantiate a binary search tree with a single root node', () => {
     const tree = new BinarySearchTree();
     tree.root = new Node(23);
+
     expect(tree.root.value).toStrictEqual(23);
     expect(tree.root.right).toStrictEqual(null);
     expect(tree.root.left).toStrictEqual(null);
@@ -22,12 +24,13 @@ describe('Testing Binary Search Tree', () => {
     tree.root = new Node(23);
     tree.root.left = new Node(8);
     tree.root.right = new Node(42);
+
     expect(tree.root.value).toStrictEqual(23);
     expect(tree.root.left.value).toStrictEqual(8);
     expect(tree.root.right.value).toStrictEqual(42);
   });
 
-  it('Can return a collection from a preorder traversal', () => {
+  it('Can return a collection from a pre-order traversal', () => {
     const tree = new BinarySearchTree();
     tree.root = new Node(23);
     tree.root.left = new Node(8);
@@ -35,10 +38,11 @@ describe('Testing Binary Search Tree', () => {
     tree.root.left.left = new Node(4);
     tree.root.left.right = new Node(16);
     tree.root.right.left = new Node(27);
+
     expect(tree.doPreOrder()).toStrictEqual([23, 8, 4, 16, 42, 27]);
   });
 
-  it('Can return a collection from a inorder traversal', () => {
+  it('Can return a collection from a in-order traversal', () => {
     const tree = new BinarySearchTree();
     tree.root = new Node(23);
     tree.root.left = new Node(8);
@@ -46,10 +50,11 @@ describe('Testing Binary Search Tree', () => {
     tree.root.left.left = new Node(4);
     tree.root.left.right = new Node(16);
     tree.root.right.left = new Node(27);
+
     expect(tree.doInOrder()).toStrictEqual([4, 8, 16, 23, 27, 42]);
   });
 
-  it('Can return a collection from a postorder traversal', () => {
+  it('Can return a collection from a post-order traversal', () => {
     const tree = new BinarySearchTree();
     tree.root = new Node(23);
     tree.root.left = new Node(8);
@@ -57,7 +62,30 @@ describe('Testing Binary Search Tree', () => {
     tree.root.left.left = new Node(4);
     tree.root.left.right = new Node(16);
     tree.root.right.left = new Node(27);
+
     expect(tree.doPostOrder()).toStrictEqual([4, 16, 8, 27, 42, 23]);
+  });
+
+  it('Can add a new node in the correct location in the binary search tree', () => {
+    const tree = new BinarySearchTree();
+    tree.root = new Node(23);
+    tree.root.left = new Node(8);
+    tree.root.right = new Node(42);
+    tree.root.left.left = new Node(4);
+    tree.root.left.right = new Node(16);
+    tree.root.right.left = new Node(27);
+    // tree.add(6);
+
+    expect(tree.contains(16)).toBeTruthy();
+  });
+
+  it('Can indicate whether or not the value is in the tree at least once', () => {
+    const tree = new BinarySearchTree();
+    tree.root = new Node(23);
+    tree.root.left = new Node(8);
+
+    expect(tree.contains(8)).toStrictEqual(true);
+    expect(tree.contains(400)).toStrictEqual(false);
   });
 
 });
