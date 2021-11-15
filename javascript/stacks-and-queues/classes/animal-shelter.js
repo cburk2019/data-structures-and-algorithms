b7ad9085bbfc1a3ebaa1f2c2703391c8c47bc38e
Christopher Burk <c.burk.tech@gmail.com>
2021-11-15 00:38:05 -0800

    adds code to complete animal shelter challenge with appropriate test suite, all tests passing

--- javascript/stacks-and-queues/classes/animal-shelter.js
+++ javascript/stacks-and-queues/classes/animal-shelter.js
@@ -1,1 +1,46 @@
 'use strict';
+
+const Queue = require('./queue.js');
+
+class AnimalShelter {
+  constructor() {
+    this.dogs = new Queue;
+    this.cats = new Queue;
+  }
+
+  // adds a new Node to the back of queue
+  enqueue(animalObj) {
+    // animal object can be either a dog or a cat
+    try {
+      if (animalObj.type !== 'dog' && animalObj.type !== 'cat') {
+        throw new Error('Partner, please enter either dog or cat');
+      }
+
+      if (animalObj.type === 'dog') {
+        this.dogs.enqueue(animalObj);
+        return;
+      }
+      this.cats.enqueue(animalObj);
+    } catch (error) {
+      throw `Sorry partner, unable to add a new animal to the back of shelter queue: ${error}`;
+    }
+  }
+
+  // removes the node from the front of the queue
+  dequeue(pref) {
+    // Return: either a dog or a cat, based on preference
+    try {
+      if (pref !== 'dog' && pref !== 'cat') {
+        return null;
+      }
+      if (pref === 'dog') {
+        return this.dogs.dequeue();
+      }
+      return this.cats.dequeue();
+    } catch (error) {
+      throw `Sorry partner, unable to remove animal from front of shelter queue: ${error}`;
+    }
+  }
+}
+
+module.exports = AnimalShelter;
