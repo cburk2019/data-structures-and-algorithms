6ff86f0332b98c087fd6a8000983340bc42d8d77
Christopher Burk <c.burk.tech@gmail.com>
2021-12-12 17:13:44 -0800

    updates pre-existing folder from starter code back to original, comments out code in graphs index to allow appropriate testing

--- javascript/code-challenges/reverse-ll/reverse-ll.js
+++ javascript/code-challenges/reverse-ll/reverse-ll.js
@@ -1,16 +1,7 @@
 'use strict';
 
-// Require the linked list implementation
-const LinkedList = require('../../401/linked-list/index');
+class LinkedList {
 
+}
 
-/*
-  Accept a linked list
-  Reveres it
-  Return it
-*/
-module.exports = function reverse(list) {
-
-  return true;
-
-};
+module.exports = LinkedList;
